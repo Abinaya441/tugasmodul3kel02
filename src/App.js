84cf5f1db835c689e757c2bde6c448497621d166
Nurhaidah <63896736+nurhaidah346@users.noreply.github.com>
2022-10-27 11:17:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Satu from './components/satu';
 import Dua from './components/dua';
 import Tiga from './components/tiga';
+import Empat from './components/empat';
 
 function App() {
   return (
@@ -19,12 +20,16 @@ function App() {
             <li className="li">
               <Link to="/tiga">Roll Angka Random</Link>
             </li>
+            <li className="li">
+              <Link to="/empat">Guess Number</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" exact element={<Satu />} />
           <Route path="/dua" exact element={<Dua />} />
           <Route path="/tiga" exact element={<Tiga />} />
+          <Route path="/empat" exact element={<Empat />} />
         </Routes>
       </div>
     </Router>
